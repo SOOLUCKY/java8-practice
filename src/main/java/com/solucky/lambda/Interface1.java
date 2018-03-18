3c7f684d064f6c177c592dee86cb84f73d2d4c95
SOOLUCKY <1660255215@qq.com>
2018-03-18 11:48:22 +0800

    interface

---
+++ src/main/java/com/solucky/lambda/Interface1.java
@@ -0,0 +1,46 @@
+package com.solucky.lambda;
+
+import static java.lang.Math.sqrt;
+
+/**
+ * Created by xiechunlu on 2018-03-18 上午11:27
+ */
+
+public class Interface1 {
+
+    interface Formula {
+        double calculate(int a);
+
+        /**
+         * 默认接口实现
+         *
+         * @param a
+         * @return
+         */
+        default double sqrt(int a) {
+            return sqrt(positive(a));
+        }
+
+        static int positive(int a) {
+            return a > 0 ? a : 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        Formula formula1 = new Formula() {
+            @Override
+            public double calculate(int a) {
+                return sqrt(a * 100);
+            }
+        };
+
+        formula1.sqrt(-23);
+        Formula.positive(-4);
+        formula1.sqrt(1);
+
+//        lambda方式实现接口
+        Formula formula2 = (a) -> sqrt(a * 100);
+    }
+
+
+}
